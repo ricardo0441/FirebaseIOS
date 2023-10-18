86d1eedd1d8e772662e28039f647c4e32512ccba
ricardo0441 <ricardo0441@gmail.com>
2023-10-18 15:54:24 -0600

    Initial Commit

---
+++ FirebaseIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseIOS
+//
+//  Created by Ricardo Rodriguez on 10/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
